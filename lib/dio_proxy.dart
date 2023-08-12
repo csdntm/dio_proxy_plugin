2bb80786016f9a8dfa9e8e715cfe29814f4f1de5
Romanbot4 <tice.lover1500@gmail.com>
2023-08-12 15:56:01 +0630

    update to compatible with latest version of dio

--- lib/dio_proxy.dart
+++ lib/dio_proxy.dart
@@ -2,18 +2,19 @@ library dio_proxy;
 
 import 'dart:io';
 
-import 'package:dio/adapter.dart';
+import 'package:dio/io.dart';
 
 /// HttpProxyAdapter
 /// [ipAddr] ip地址，默认是localhost
 /// [port] 端口，默认是8888
 ///
-class HttpProxyAdapter extends DefaultHttpClientAdapter {
+class HttpProxyAdapter extends IOHttpClientAdapter {
   final String ipAddr;
   final int port;
 
   HttpProxyAdapter({this.ipAddr = 'localhost', this.port = 8888}) {
-    onHttpClientCreate = (client) {
+    final HttpClient client = HttpClient();
+    createHttpClient = () {
       var proxy = '$ipAddr:$port';
       client.findProxy = (url) {
         return 'PROXY $proxy';
@@ -21,6 +22,7 @@ class HttpProxyAdapter extends DefaultHttpClientAdapter {
 
       client.badCertificateCallback =
           (X509Certificate cert, String host, int port) => true;
+      return client;
     };
   }
 }
